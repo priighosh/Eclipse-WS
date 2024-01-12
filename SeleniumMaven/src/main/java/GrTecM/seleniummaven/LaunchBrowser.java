97c063e8eaf9d8ed73c0d0ce1cc873dd89ec07bd
Priyanka <priyankag448@gmail.com>
2024-01-12 13:00:25 +0530

    Uploading seleniumMaven

---
+++ SeleniumMaven/src/main/java/GrTecM/seleniummaven/LaunchBrowser.java
@@ -0,0 +1,12 @@
+package GrTecM.seleniummaven;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser
+{
+   public static void main(String[] args)
+   {
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+   }
+}
---
+++ SeleniumMaven/src/main/java/testngremote/FirstprogramDay51.java
@@ -0,0 +1,12 @@
+package testngremote;
+
+import org.testng.annotations.Test;
+
+public class FirstprogramDay51
+{
+@Test
+public void TestCase1() //Import was not coming becoz it was not downloaded, go to mvn repository, download add libraries
+{
+	System.out.println("This is 1st prog of TestNG Remote");
+}
+}
